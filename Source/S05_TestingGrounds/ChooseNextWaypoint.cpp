874b055a06a6e5c6207e42cfd63af993f6e53b1d
Jonathan Bullard <jmbullard7@gmail.com>
2017-05-20 17:47:03 -0700

    Migrated AI guards patrol behavior to an ActorComponent C++ class.

---
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "S05_TestingGrounds.h"
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+	
+	/// Get the patrol route
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	UPatrolRoute* PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	/// Warn about empty patrol route
+	TArray<AActor*> PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Guard %s is missing patrol points."), *ControlledPawn->GetName());
+		return EBTNodeResult::Failed;
+	}
+
+	/// Set next waypoint
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	int Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	
+	/// Cycle the index
+	int NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	
+	return EBTNodeResult::Succeeded;
+}
+
